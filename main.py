16d5b02aa258ed76dd94f190b6fa1dd8fa5c1d1f
peaoat <squidsound@gmail.com>
2018-04-05 16:38:18 -0400

    main update

--- main.py
+++ main.py
@@ -12,14 +12,24 @@ class Entity:
         self.color = color
 
     def move(self, dx, dy) :
-        self.x += dx
-        self.y += dy
+        # If the desired tile is blocked, do not move
+        if not field[self.x + dx][self.y + dy].blocked:
+            self.x += dx
+            self.y += dy
 
     def draw(self) :
         con.draw_char(self.x, self.y, self.char, self.color)
 
     def clear(self) :
         con.draw_char(self.x, self.y, ' ', self.color)
+
+class Tile:
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        if block_sight is None:
+            block_sight = blocked
+        self.block_sight = block_sight
 
 
 def handle_keys():
@@ -39,9 +49,49 @@ def handle_keys():
     elif user_input.key == 'RIGHT':
         player.move(1, 0)
 
+
+def make_field():
+    global field
+
+    # Create a 2D array of Tiles
+    field = [
+        [Tile(False) for y in range(field_height)] for x in range(field_width)]
+
+    # Mark walls as blocked
+    field[30][22].blocked = True
+    field[30][22].block_sight = True
+    field[50][22].blocked = True
+    field[50][22].block_sight = True
+
+
+def render_all():
+    for y in range(field_height):
+        for x in range(field_width):
+            # Get the walls from the field generated in @make_field()
+            wall = field[x][y].block_sight
+            # Render walls as a different color
+            if wall:
+                con.draw_char(x, y, None, fg=None, bg=c_dark_wall)
+            else:
+                con.draw_char(x, y, None, fg=None, bg=c_dark_gnd)
+
+    for obj in objects:
+        obj.draw()
+
+    root.blit(con, 0, 0, screen_width, screen_height, 0, 0)
+
+
 # Screen size
 screen_width = 80
 screen_height = 50
+
+# Field size
+field_width = 80
+field_height = 50
+
+# Tile colors
+c_dark_wall = (0, 0, 100)
+c_dark_gnd = (50, 50, 150)
 
 # Set the font
 # tdl.set_font('arial110x10.png', greyscale=True, altLayout=True)
@@ -60,16 +110,16 @@ player = Entity(screen_width // 2, screen_height // 2, '@', (255, 255, 255))
 # Create an npc
 npc = Entity(screen_width // 2 - 5, screen_height // 2, '@', (255, 255, 0))
 
+# list all of the entities
 objects = [npc, player]
+
+# Create the field
+make_field()
 
 while not tdl.event.is_window_closed():
 
     # Write the entities to the screen
-    for obj in objects:
-        obj.draw()
-
-    # Blit the console to the display
-    root.blit(con, 0, 0, screen_width, screen_height, 0, 0)
+    render_all()
     # Push the display
     tdl.flush()
 
